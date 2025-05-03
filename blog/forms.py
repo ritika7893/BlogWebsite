0de5ea55eb25faca3cfbe92177dab9ed0117d8bf
ritika7893 <ritikakumari87344@gmail.com>
2025-05-03 17:29:45 +0530

    Ready

---
+++ blog/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+
+class CommentForm(forms.Form):
+    author = forms.CharField(
+        max_length=255,
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "your name"}
+        ),
+    )
+    body = forms.CharField(
+        widget=forms.Textarea(
+            attrs={"class": "form-control", "placeholder": "Leave A Comment"}
+        )
+    )
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from blog.models import Post, Comment, Category
+from blog.forms import CommentForm
 
 
 # Create your views here.
@@ -20,7 +22,19 @@ def blog_category(request, category):
 
 
 def blog_detail(request, pk):
-    post = Post.objects.get(pk=id)
+    post = Post.objects.get(pk=pk)
+    form = CommentForm()
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post,
+            )
+            comment.save()
+            return HttpResponseRedirect(request.path_info)
+
     comments = Comment.objects.filter(post=post)
-    context = {"posts": post, "comments": comments}
-    return render(request, "blog/category.html", context)
+    context = {"post": post, "comments": comments, "form": form}
+    return render(request, "blog/blog_detail.html", context)
